828c42953962ff50ede1a35f6c63d5608e506abe
NiveaRoy <150788932+NiveaRoy@users.noreply.github.com>
2025-10-12 20:51:08 +1100

    Add files via upload

---
+++ classification/Binary_early_vs_advanced/ResNet50_pretrained/code/pre_rec_sn_sp.py
@@ -0,0 +1,27 @@
+# ==============================================================
+# Code to calculate Precision, Reacll, Sensitivity, Specificity
+# ==============================================================
+import pandas as pd
+
+df_preds = pd.read_csv("path/to/test_predictions_resnet50_TL.csv")
+y_true = df_preds["Label"].values
+y_scores = df_preds["Probability"].values  # Raw predicted probabilities
+
+#=====================
+
+import numpy as np
+from sklearn.metrics import precision_score, recall_score, confusion_matrix
+
+threshold = 0.5
+y_pred = (y_scores >= threshold).astype(int)
+
+precision = precision_score(y_true, y_pred)
+recall = recall_score(y_true, y_pred)
+tn, fp, fn, tp = confusion_matrix(y_true, y_pred).ravel()
+sensitivity = tp / (tp + fn)
+specificity = tn / (tn + fp)
+
+print(f"Precision: {precision:.3f}")
+print(f"Recall: {recall:.3f}")
+print(f"Sensitivity: {sensitivity:.3f}")
+print(f"Specificity: {specificity:.3f}")
